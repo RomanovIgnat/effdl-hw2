b92255d71b7c32d756cb00cd186357a42018d94a
RomanovIgnat <romanov.ignat.p@gmail.com>
2023-02-23 18:26:54 +0300

    fix_train

--- section1/train.py
+++ section1/train.py
@@ -1,8 +1,10 @@
 import torch
 from torch import nn
 from tqdm.auto import tqdm
+import click
 
 from unet import Unet
+from scaler import StaticScaler, DynamicScaler
 
 from dataset import get_train_data
 
@@ -12,6 +14,7 @@ def train_epoch(
     model: torch.nn.Module,
     criterion: torch.nn._Loss,
     optimizer: torch.optim.Optimizer,
+    scaler,
     device: torch.device,
 ) -> None:
     model.train()
@@ -24,21 +27,36 @@ def train_epoch(
         with torch.cuda.amp.autocast():
             outputs = model(images)
             loss = criterion(outputs, labels)
-        # TODO: your code for loss scaling here
+
+        optimizer.zero_grad()
+        scaler.scale(loss).backward()
+        scaler.step(optimizer)
+        scaler.update()
 
         accuracy = ((outputs > 0.5) == labels).float().mean()
 
         pbar.set_description(f"Loss: {round(loss.item(), 4)} " f"Accuracy: {round(accuracy.item() * 100, 4)}")
 
 
-def train():
+@click.command()
+@click.option('--grad-scaler',
+              type=click.Choice(['static', 'dynamic'], case_sensitive=False),
+              default='static'
+              )
+def train(grad_scaler):
     device = torch.device("cuda:0")
     model = Unet().to(device)
     criterion = nn.BCEWithLogitsLoss()
     optimizer = torch.optim.Adam(model.parameters(), lr=1e-4)
 
+    scaler = StaticScaler() if grad_scaler == 'static' else DynamicScaler()
+
     train_loader = get_train_data()
 
     num_epochs = 5
     for epoch in range(0, num_epochs):
-        train_epoch(train_loader, model, criterion, optimizer, device=device)
+        train_epoch(train_loader, model, criterion, optimizer, scaler, device=device)
+
+
+if __name__ == '__main__':
+    train()
